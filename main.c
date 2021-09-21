5b54807ab380c7be9478cba7490ca6cc83a4a4c5
Sulaiman Kasas <sulaimankasas@Sulaimans-MacBook-Pro.local>
2021-09-21 13:37:53 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h> //fflush()
+
+
+
+
+
+void init_shell()
+{
+    printf("..............\n"
+           "Shell started\n"
+           "..............\n");
+}
+
+int main() {
+    init_shell();
+    return 0;
+}
